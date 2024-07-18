f4abc3e5abf8416f0b0e56afea381e5cc93c7681
AhmedElaidy <alaidy.y123@gmail.com>
2024-07-18 14:18:14 +0300

    download page arabic and english is done just testing left

--- JS/download-center.js
+++ JS/download-center.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const subCheckboxes = document.querySelectorAll(".sub-checkbox");
   const downloadAllButton = document.getElementById("download-all");
   const downloadSelectedButton = document.getElementById("download-selected");
+  const langButton = document.getElementById("languageToggle");
+
+  langButton.addEventListener("click", () => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+  });
 
   sectionHeaders.forEach((header) => {
     header.addEventListener("change", () => {
@@ -26,13 +34,32 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   downloadAllButton.addEventListener("click", () => {
-    downloadFiles(["/pdfs/التقرير كامل.pdf"]);
+    const bodyLang = document.body.lang;
+
+    if (bodyLang === "en") {
+      downloadFiles(["/pdfs/Full Report.pdf"]);
+    } else {
+      console.log("inside arabic full report");
+      downloadFiles(["/pdfs/التقرير كامل.pdf"]);
+    }
   });
 
   downloadSelectedButton.addEventListener("click", () => {
-    const selectedFiles = Array.from(
-      document.querySelectorAll(".sub-checkbox:checked")
-    ).map((cb) => cb.dataset.file);
+    const bodyLang = document.body.lang;
+    let selectedFiles;
+
+    if (bodyLang === "en") {
+      selectedFiles = Array.from(
+        document.querySelectorAll(".english-item:checked")
+      ).map((cb) => cb.dataset.file);
+      console.log("selectedFiles are ", selectedFiles);
+    } else {
+      selectedFiles = Array.from(
+        document.querySelectorAll(".arabic-item:checked")
+      ).map((cb) => cb.dataset.file);
+      console.log("selectedFiles are ", selectedFiles);
+    }
+
     downloadFiles(selectedFiles);
   });
 
